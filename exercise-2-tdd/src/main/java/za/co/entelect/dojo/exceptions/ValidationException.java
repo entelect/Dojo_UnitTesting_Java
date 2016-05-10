4a665b35c7063a8dd44e234c0032fa16e8ba1d96
GrantM <grantmarais@gmail.com>
2016-05-10 23:10:10 +0200

    build fix

--- exercise-2-tdd/src/main/java/za/co/entelect/dojo/exceptions/ValidationException.java
+++ exercise-2-tdd/src/main/java/za/co/entelect/dojo/exceptions/ValidationException.java
@@ -1,12 +1,13 @@
 package za.co.entelect.dojo.exceptions;
 
-import za.co.entelect.dojo.validation.enums.ResponseEnum;
+
+import za.co.entelect.dojo.enums.ResponseEnum;
 
 public class ValidationException extends RuntimeException {
-    ResponseEnum responseEnum;
+
+    private ResponseEnum responseEnum;
 
     public ValidationException(ResponseEnum aResponseEnum) {
-        super();
         responseEnum = aResponseEnum;
     }
 }
