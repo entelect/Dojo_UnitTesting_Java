94a1e874a2a2036c7d8b518bc3f834ec112b2e07
pieter56 <pieterbo@discovery.co.za>
2016-06-09 14:54:28 +0200

    Some extra card numbers

--- exercise-1-fix/src/test/java/za/co/entelect/dojo/services/CardValidationImplTest.java
+++ exercise-1-fix/src/test/java/za/co/entelect/dojo/services/CardValidationImplTest.java
@@ -7,7 +7,12 @@ import org.junit.Test;
 public class CardValidationImplTest {
 
     private static final String VALID_CARD_NUMBER_1 = "4388576018410707";
-    private static final String VALID_CARD_NUMBER_2 = "6304219447607087665";
+    private static final String VALID_CARD_NUMBER_2 = "4024007139789647";
+    private static final String VALID_CARD_NUMBER_3 = "4024007133504414";
+    private static final String VALID_CARD_NUMBER_4 = "4485922140503070";
+    private static final String VALID_CARD_NUMBER_5 = "4024007100687804";
+    private static final String VALID_CARD_NUMBER_6 = "4860068968802593";
+    private static final String VALID_CARD_NUMBER_7 = "6304219447607087665";
     private static final String INVALID_CARD_NUMBER = "4388576018402626";
 
     //@todo write some tests
