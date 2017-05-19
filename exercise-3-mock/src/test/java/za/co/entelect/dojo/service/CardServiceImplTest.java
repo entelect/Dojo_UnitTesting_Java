a9e644d8f1fe5b0eaaeef7bb5a13247e423f5c4e
gamarais <grantmarais@gmail.com>
2017-05-19 09:54:46 +0200

    updated card number

--- exercise-3-mock/src/test/java/za/co/entelect/dojo/service/CardServiceImplTest.java
+++ exercise-3-mock/src/test/java/za/co/entelect/dojo/service/CardServiceImplTest.java
@@ -90,7 +90,7 @@ public class CardServiceImplTest {
         if (!validCardNumber) {
             cardNumber = "4929900481403642";
         }
-        String track2 = cardNumber + "1703101";
+        String track2 = cardNumber + "1703";
         String pinBlock = "FF" + cardNumber;
         return new Card(track2,pinBlock, balanceAvailableInCents);
     }
